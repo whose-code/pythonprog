7ec98e5ea77a9d799d7c932f1aeea5ff017c3190
whose-code <49828191+shaheerxt@users.noreply.github.com>
2021-06-17 22:58:19 +0200

    even or odd

---
+++ 3.4.py
@@ -0,0 +1,16 @@
+x=[1,5,20,60/7]
+print(x)
+for i in x:
+    if i%2==0:
+        print (f"{i} is even")
+    else:
+        print(f"{i} is odd")
+
+"""
+output:
+[1, 5, 20, 8.571428571428571]
+1 is odd
+5 is odd
+20 is even
+8.571428571428571 is odd
+"""
